ea64e620da8f5ddabb7dfe722fa3b72cf5c47f4d
Changming Sun <chasun@microsoft.com>
2020-03-31 08:35:30 -0700

    a

--- onnxruntime/test/onnx/microbenchmark/main.cc
+++ onnxruntime/test/onnx/microbenchmark/main.cc
@@ -9,12 +9,14 @@
 #include <core/providers/cpu/cpu_execution_provider.h>
 #include "core/session/environment.h"
 #include <core/common/logging/sinks/clog_sink.h>
+#include <core/platform/Barrier.h>
 #include <core/graph/model.h>
 #include <core/graph/graph.h>
 #include <core/framework/kernel_def_builder.h>
 #include <core/session/onnxruntime_c_api.h>
 #include <core/session/onnxruntime_cxx_api.h>
 #include <core/session/ort_env.h>
+#include <core/util/thread_utils.h>
 
 #include <unordered_map>
 
@@ -75,6 +77,77 @@ static void BM_CreateThreadPool(benchmark::State& state) {
   }
 }
 BENCHMARK(BM_CreateThreadPool)->UseRealTime()->Unit(benchmark::TimeUnit::kMillisecond);
+
+//On Xeon W-2123 CPU, it takes about 2ns for each iteration
+#ifdef _WIN32
+#pragma optimize( "", off )
+#else
+#pragma GCC push_options
+#pragma GCC optimize ("O0")
+#endif
+void SimpleForLoop(ptrdiff_t first, ptrdiff_t last){
+    size_t sum = 0;
+    for(;first!=last;++first){
+        ++sum;
+    }
+}
+#ifdef _WIN32
+#pragma optimize( "", on )
+#else
+#pragma GCC pop_options
+#endif
+
+static void BM_ThreadPoolParallelFor(benchmark::State& state) {
+  const size_t len = state.range(0);
+  OrtThreadPoolParams tpo;
+  std::unique_ptr<concurrency::ThreadPool> tp(concurrency::CreateThreadPool(&onnxruntime::Env::Default(), tpo,nullptr));
+  for (auto _ : state) {
+    tp->ParallelFor(len,200,SimpleForLoop);
+  }
+}
+BENCHMARK(BM_ThreadPoolParallelFor)->UseRealTime()->Unit(benchmark::TimeUnit::kNanosecond)->Arg(100)->Arg(1000)->Arg(10000)->Arg(20000)->Arg(40000)->Arg(80000);
+
+static void BM_SimpleForLoop(benchmark::State& state) {
+  const size_t len = state.range(0);
+  for (auto _ : state) {
+   SimpleForLoop(0,len);
+  }
+}
+BENCHMARK(BM_SimpleForLoop)->Unit(benchmark::TimeUnit::kNanosecond)->Arg(100);
+
+
+static void TestPartitionWork(std::ptrdiff_t ThreadId, std::ptrdiff_t ThreadCount, std::ptrdiff_t TotalWork,
+                            std::ptrdiff_t* WorkIndex, std::ptrdiff_t* WorkRemaining) {
+    const std::ptrdiff_t WorkPerThread = TotalWork / ThreadCount;
+    const std::ptrdiff_t WorkPerThreadExtra = TotalWork % ThreadCount;
+
+    if (ThreadId < WorkPerThreadExtra) {
+      *WorkIndex = (WorkPerThread + 1) * ThreadId;
+      *WorkRemaining = WorkPerThread + 1;
+    } else {
+      *WorkIndex = WorkPerThread * ThreadId + WorkPerThreadExtra;
+      *WorkRemaining = WorkPerThread;
+    }
+  }
+
+static void BM_SimpleScheduleWait(benchmark::State& state) {
+  const size_t len = state.range(0);
+  OrtThreadPoolParams tpo;
+  std::unique_ptr<concurrency::ThreadPool> tp(concurrency::CreateThreadPool(&onnxruntime::Env::Default(), tpo,nullptr));
+  size_t threads = tp->NumThreads();
+
+  for (auto _ : state) {
+    onnxruntime::Barrier barrier(static_cast<unsigned int>(len));
+     for (std::ptrdiff_t id = 0; id < threads; ++id) {
+         tp->Schedule([id,threads,len](){
+              std::ptrdiff_t start, work_remaining;
+              TestPartitionWork(id, threads, len, &start, &work_remaining);
+              SimpleForLoop(start,start+work_remaining);
+         });
+     }  
+  }
+}
+BENCHMARK(BM_SimpleScheduleWait)->UseRealTime()->Unit(benchmark::TimeUnit::kNanosecond)->Arg(100)->Arg(1000)->Arg(10000)->Arg(20000)->Arg(40000)->Arg(80000);
 
 int main(int argc, char** argv) {
   ::benchmark::Initialize(&argc, argv);
